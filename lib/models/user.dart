660b7d5875b91db270a2c1be3a63709f31ac99ec
MuradMagomedov <magomedoff84@gmail.com>
2023-03-09 10:02:07 +0300

    fix модель постов

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -3,18 +3,31 @@ class User {
   final String name;
   final String username;
   final String email;
+  final String adress;
+  final String website;
+  final String phone;
+  final String company;
 
   User({
     required this.id,
     required this.name,
     required this.username,
     required this.email,
+    required this.adress,
+    required this.website,
+    required this.phone,
+    required this.company,
   });
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
-        id: json['id'],
-        name: json['name'],
-        username: json['username'],
-        email: json['email']);
+      id: json['id'],
+      name: json['name'],
+      username: json['username'],
+      email: json['email'],
+      adress: json['adress'],
+      website: json['website'],
+      phone: json['phone'],
+      company: json['company'],
+    );
   }
 }
